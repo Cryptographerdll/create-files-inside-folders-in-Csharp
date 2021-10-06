9da52d136f5dbfb2ae8c05c9e82dadc4dc1b3528
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-10-06 15:57:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+
+
+namespace WriteFile_Fichier
+{
+    class Program
+    {
+        // How To create A file inside folder and edit it by writing text inside it .
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fichier = path + Path.DirectorySeparatorChar + "Fichier" + Path.DirectorySeparatorChar + "khalil.txt";
+
+            if(!File.Exists(fichier))
+            {
+                string txt = "Welcome to khalil world " + Environment.NewLine;
+                File.WriteAllText(fichier, txt);
+            }
+            else
+            {
+                string txt = "New Line" + Environment.NewLine;
+                File.AppendAllText(fichier, txt);
+            }
+            Console.ReadLine();
+        }
+    }
+}
